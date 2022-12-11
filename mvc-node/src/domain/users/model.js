f964f2d7c1f1e1fda328f2669b378f0ebee29f4b
illia-git <ichykurov@gmail.com>
2022-12-11 19:41:37 +0100

    initial commit + first config of mvc

---
+++ mvc-node/src/domain/users/model.js
@@ -0,0 +1,38 @@
+let users = [
+    {id: '1', username: 'test', age: '5'}
+];
+
+module.exports = {
+    create: ({username, age}) => {
+        const newUser = {
+            username,
+            age,
+            id: String(Date.now())
+        }
+
+        if (!users.find(user => user.username === users)) {
+            users.push(newUser);
+        } else {
+            throw new Error('User already exists');
+        }
+
+        return newUser;
+    },
+    removeById: ({id}) => {
+        const userIndex = users.findIndex(user => user.id === String(id));
+
+        if (userIndex === -1) {
+            throw new Error('User does not exists');
+        }
+
+        users.splice(userIndex, 1);
+
+        return id;
+    },
+    removeByUsername: ({username}) => {
+    },
+    getAll: () => {
+    },
+    getById: ({id}) => {
+    }
+}
